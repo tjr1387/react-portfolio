c5853e12c9f6c751ec5308b65d31f5bf341291df
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-06 16:09:40 -0500

    Add basic form structure to Contact component

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,2 +1,23 @@
 import React from 'react';
 
+export default function Contact() {
+  return (
+    <form action="#" class="">
+      <div class="">
+        <label for="email-name">Name: </label>
+        <input type="text" name="email-name" id="email-name" required />
+      </div>
+      <div class="">
+        <label for="email-address">Email Address: </label>
+        <input type="email" name="email-address" id="email-address" required />
+      </div>
+      <div class="">
+        <label for="email-message">Message: </label>
+        <textarea name="email-message" id="email-message" rows="6" required></textarea>
+      </div>
+      <button>
+        Send
+      </button>
+    </form>
+  );
+}
