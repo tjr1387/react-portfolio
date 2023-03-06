c2150bb7c356f84307b69e1e3f502b30036f3920
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-06 15:49:16 -0500

    Add content to About component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,2 +1,19 @@
 import React from 'react';
 
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      {/* some dummy image */}
+      <img src="#" />
+      <p> I was born in Canada, about an hour west of Vancouver, B.C. My father was a Boston sports fan, and I grew to become one also.
+        In 1996, he took a job interview in Massachusetts and was offered the position.
+        <br></br>
+        Growing up I was a relatively serious competetive gymnast, doing it through high school graduation. After graduating from Bishop Guertin (Nashua, NH),
+        I attended the College of William and Mary; I was there for roughly 3 years, quitting gymnastics pretty early into that stint, and dropping out!
+        I moved back up to New England in 2009 to do what college dropouts in niche sports wind up doing -- coaching that sport. I spent roughly 12 years coaching gymnastics,
+        knowing that I eventually would need to do something else if I ever wanted to own a house or have a family. I am now working towards becoming a credible web developer. Fingers crossed!
+      </p>
+    </div>
+  );
+}
