84113f4b25b9785a9e3df784e6a70755c60f9508
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-06 15:22:32 -0500

    Add structure/logic to Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-// will be inside the Header component, and act as links to different 'pages'
-  // 1. About Me
-  // 2. Portfolio (the projects)
-  // 3. Contact
-  // 4. Resume
+function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <ul className="">
+      <li className="">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="">
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+  
+export default Navigation;
