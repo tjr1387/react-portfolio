dc2bcd3356a32ebdd1c2042d94f6ac1cc681f9de
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-06 16:45:55 -0500

    Add basic structure to Resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,2 +1,24 @@
 import React from 'react';
 
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      {/* Will need to make this a link to DL a resume file */}
+      <p>Download my <span>resume</span></p>
+      
+      <h2>Front-end Proficencies</h2>
+      <ul>
+        <li>Impossible</li>
+        <li>to</li>
+        <li>tell</li>
+      </ul>
+      <h2>Back-end Proficencies</h2>
+      <ul>
+        <li>Absolutely</li>
+        <li>no</li>
+        <li>idea</li>
+      </ul>
+    </div>
+  );
+}
