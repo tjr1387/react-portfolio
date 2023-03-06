42aa48942440505ae5e6214f5c97480afc3cb9a3
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-06 17:44:00 -0500

    Add blueprint structure to Project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,15 @@
 import React, { useState } from 'react';
 
-// the component for each project page link in the 'portfolio' section
+// the component for each project page link in the 'portfolio' page
+// will take props
 
+export default function Project(props) {    // should destructure fields out instead   
+  return (
+    <div class="project-item" style={`background-image: url("imgSrc prop")`}>
+      <p class="project-title">Title Prop</p>
+      <p class="project-desc">Description Prop</p>
+      <a href="`repo prop`">Repo</a>
+      <a href="`livesite prop`">Site/Video</a>
+    </div>
+  );
+}
