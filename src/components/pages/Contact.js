6dc95c5205f65d5081da8998d5aae7cce8cc95b4
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-09 16:10:03 -0500

    Add invalid email error msg

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,22 +9,35 @@ function handleBlur(event) {
   }
 }
 
+const emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
+function validateEmail() {
+  const emailInputEl = document.querySelector('#email-address');
+  const emailMsgEl = document.querySelector('#error-msg-email');
+  if (emailRegex.test(emailInputEl.value)) {
+    emailMsgEl.innerText = '';
+  } else {
+    emailMsgEl.innerText = 'Please enter a valid email address';
+  }
+};
+
 export default function Contact() {
   return (
-    <form action="#" className="">
+    <form action="#" className="" onSubmit={(e) => e.preventDefault()}>
       <div className="">
         <label htmlFor="email-name">Name: </label>
         <input type="text" name="email-name" id="email-name" onBlur={(e) => handleBlur(e)} required />
       </div>
       <div className="">
         <label htmlFor="email-address">Email Address: </label>
-        <input type="email" name="email-address" id="email-address" onBlur={(e) => handleBlur(e)} required />
+        <input type="email" name="email-address" id="email-address" onBlur={(e) => handleBlur(e)} onChange={validateEmail} required />
+        <p className="error-text" id="error-msg-email"></p>
       </div>
       <div className="">
         <label htmlFor="email-message">Message: </label>
         <textarea name="email-message" id="email-message" rows="6" onBlur={(e) => handleBlur(e)} required></textarea>
       </div>
-      <div id="error-msg-blank"></div>
+      <div className="error-text" id="error-msg-blank"></div>
       <button>
         Send
       </button>
