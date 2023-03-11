a491373f00be155bcba4d1ec443ce7d0a906db62
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-11 12:12:42 -0500

    Add project photos and project links

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ export default function Project(props) {
     <div key={props.key} className="project-item" style={{ backgroundImage: `url(${props.imgSrc})` }}>
       <a className="project-title" href={props.liveSiteUrl}>{props.title}</a>
       <p className="project-desc">{props.description}</p>
-      <a href={props.gitHubUrl}>Repo</a>
+      <a className="project-github" href={props.gitHubUrl}>REPO</a>
     </div>
   );
 }
