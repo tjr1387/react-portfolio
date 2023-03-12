99d11e4aa06f4d82474fd92e9c239a5aadbfac92
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-12 10:35:55 -0400

    Make anchor tags open in new tab on Footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,17 @@ export default function Footer() {
     <footer>
       <ul className="footer-list">
         <li className="footer-item">
-          <a href="https://github.com/tjr1387">
+          <a href="https://github.com/tjr1387" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
         </li>
         <li className="footer-item">
-          <a href="https://www.linkedin.com/">
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
             LinkedIn
           </a>
         </li>
         <li className="footer-item">
-          <a href="https://www.google.com/">
+          <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
             Other
           </a>
         </li>
