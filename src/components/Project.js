d77e73a521d483d0c5d79824c12942eb76bb381d
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-03-12 10:42:17 -0400

    Make anchor tags open in new tab on Portfolio page

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Project(props) {
   return (
     <div key={props.key} className="project-item" style={{ backgroundImage: `url(${props.imgSrc})` }}>
-      <a className="project-title" href={props.liveSiteUrl}>{props.title}</a>
+      <a className="project-title" href={props.liveSiteUrl} target="_blank" rel="noopener noreferrer">{props.title}</a>
       <p className="project-desc">{props.description}</p>
-      <a className="project-github" href={props.gitHubUrl}>REPO</a>
+      <a className="project-github" href={props.gitHubUrl} target="_blank" rel="noopener noreferrer">REPO</a>
     </div>
   );
 }
